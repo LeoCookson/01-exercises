87b275883d64c6ab217c5f73192f884d7b3e3d77
LeoCookson <leocookson@outlook.com>
2018-03-06 01:51:22 +1300

    First

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace first_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
